b893a2b5219f2c3b7ff5c5d5bc25aa5764b04bc6
RBLU <reto.blunschi@youpers.com>
2017-11-21 21:22:47 +0100

    beautify

--- src/app/core/monitoring.service.ts
+++ src/app/core/monitoring.service.ts
@@ -13,7 +13,7 @@ export class MonitoringService {
 
   getRuns(runStatusFilter, nameFilter, dateFilter): Observable<[Run]> {
     console.log('filters: name:' + nameFilter + ', status: ' + runStatusFilter + ", date: " + dateFilter);
-    let url = '/runs?expand=itsMonitoring&orderBy=started|desc';
+    let url = '/runs?expand=itsMonitoring&orderBy=started' + QUERY_SEPARATOR + 'desc';
 
     if (runStatusFilter) {
       url += '&filter=runStatus' + QUERY_SEPARATOR + 'eq' + QUERY_SEPARATOR + runStatusFilter;
@@ -22,8 +22,8 @@ export class MonitoringService {
       url += '&filter=Monitoring.name' + QUERY_SEPARATOR + 'likei' + QUERY_SEPARATOR + nameFilter;
     }
     if (dateFilter) {
-      url += '&filter=started'+ QUERY_SEPARATOR + '>' + QUERY_SEPARATOR + dateFilter.toISOString();
-      url += '&filter=started'+ QUERY_SEPARATOR + '<' + QUERY_SEPARATOR +  moment(dateFilter).add(1, 'day').toISOString();
+      url += '&filter=started' + QUERY_SEPARATOR + '>' + QUERY_SEPARATOR + dateFilter.toISOString();
+      url += '&filter=started' + QUERY_SEPARATOR + '<' + QUERY_SEPARATOR + moment(dateFilter).add(1, 'day').toISOString();
     }
     return Observable.timer(0, 60000)
       .distinctUntilChanged()
